8c44878de7a7a8cc6816e945e57ae77ae1a421ed
Mateusz Kunkel <matiqk@wp.pl>
2023-02-15 20:11:34 +0100

    code in workable condition

--- app.py
+++ app.py
@@ -6,36 +6,37 @@ app = Flask(__name__)
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data = response.json()
+list_of_rates = data[0]["rates"]
 
 
-with open('requestedvalues.csv', 'w', newline='') as csvfile:
-    fieldnames = ["currency", "code", "bid", "ask"]
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=";")
+def create_CSV():
+    list_of_dic = []
+    with open("requestedvalues.csv", "w", newline="") as csvfile:
+        fieldnames = list(list_of_rates[0].keys())
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=";")
 
-    writer.writeheader()
-    for i in range(0, len(data[0]["rates"])):
+        writer.writeheader()
+        for i in range(0, len(list_of_rates)):
+            writer.writerow(list_of_rates[i])
+            list_of_dic.append(list_of_rates[i])
+            
+        return list_of_dic
 
-        writer.writerow(data[0]["rates"][i])
 
-
-@app.route('/mypage/calculator', methods=['GET', 'POST'])
-def func():
-    last_request = 0
+@app.route("/", methods=["GET", "POST"])
+def func():  
+    input_options = create_CSV()
+    last_request = ""
 
     if request.method == "POST":
         request_data = request.form
         waluta = request_data.get("waluta")
-        ilosc = int(request_data.get("ilosc"))
+        ilosc = float(request_data.get("ilosc"))
+        swap = waluta.split(":")
 
-        for i in range(0, len(data[0]["rates"])):
-            if data[0]["rates"][i]["code"] == waluta:
-                print(data[0]["rates"][i]["ask"] * ilosc)
-                last_request = data[0]["rates"][i]["ask"] * ilosc
-        
-        return f"przelicznik waluty wyniósł {last_request} zł"
+        last_request = f"Kupując {ilosc} waluty {swap[0]} zapłacisz {float(swap[1]) * ilosc} zł"
 
-    #elif request.method == "GET":
-    return render_template("example.html")
-    
+    return render_template("input.html", options = input_options, last = last_request)
 
-
+if __name__ == "__main__":
+    app.run(debug=True)
