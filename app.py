731692b1bf352221df7c2dfcee16aa74fc1dac40
Mateusz Kunkel <matiqk@wp.pl>
2023-02-27 15:24:15 +0100

    fixed internal error, loop in line19

--- app.py
+++ app.py
@@ -6,36 +6,37 @@ app = Flask(__name__)
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data = response.json()
-list_of_rates = data[0]["rates"]
 
 
-def create_CSV():
+def create_csv():
+    list_of_rates = data[0]["rates"]
     list_of_dic = []
     with open("requestedvalues.csv", "w", newline="") as csvfile:
         fieldnames = list(list_of_rates[0].keys())
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=";")
 
         writer.writeheader()
-        for i in range(0, len(list_of_rates)):
-            writer.writerow(list_of_rates[i])
-            list_of_dic.append(list_of_rates[i])
+        for i in list_of_rates:
+            writer.writerow(i)
+            list_of_dic.append(i)
             
         return list_of_dic
 
 
 @app.route("/", methods=["GET", "POST"])
 def func():  
-    input_options = create_CSV()
+    input_options = create_csv()
     last_request = ""
 
     if request.method == "POST":
         request_data = request.form
         waluta = request_data.get("waluta")
-        ilosc = float(request_data.get("ilosc"))
         swap = waluta.split(":")
-
-        last_request = f"Kupując {ilosc} waluty {swap[0]} zapłacisz {float(swap[1]) * ilosc} zł"
-
+        if request_data.get("ilosc") != "":
+            ilosc = float(request_data.get("ilosc"))
+            last_request = f"Kupując {ilosc} waluty {swap[0]} zapłacisz {float(swap[1]) * ilosc} zł"
+        else:
+            last_request = f"Radzę wpisać ile waluty zamierzasz kupić..."
     return render_template("input.html", options = input_options, last = last_request)
 
 if __name__ == "__main__":
