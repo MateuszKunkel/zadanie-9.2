64e3f395cce720f59b13472034dd2bd6b048e698
Mateusz Kunkel <matiqk@wp.pl>
2023-03-09 21:39:33 +0100

    blacked

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ import csv
 
 app = Flask(__name__)
 
-response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-data = response.json()
-
 
 def create_csv():
+    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+    data = response.json()
     list_of_rates = data[0]["rates"]
     list_of_dic = []
     with open("requestedvalues.csv", "w", newline="") as csvfile:
@@ -19,12 +18,12 @@ def create_csv():
         for i in list_of_rates:
             writer.writerow(i)
             list_of_dic.append(i)
-            
+
         return list_of_dic
 
 
 @app.route("/", methods=["GET", "POST"])
-def func():  
+def func():
     input_options = create_csv()
     last_request = ""
 
@@ -37,7 +36,8 @@ def func():
             last_request = f"Kupując {ilosc} waluty {swap[0]} zapłacisz {float(swap[1]) * ilosc} zł"
         else:
             last_request = f"Radzę wpisać ile waluty zamierzasz kupić..."
-    return render_template("input.html", options = input_options, last = last_request)
+    return render_template("input.html", options=input_options, last=last_request)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
